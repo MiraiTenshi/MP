1e2e80b2984a5c66910cbda6db7a03e3929085ac
Astrelle <anyuta.se@gmail.com>
2013-07-31 18:24:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-window.onload = function(){
-	var current_date=new Date();
-	var dataset = [];
-	/* document.getElementById("start_date").value=current_date.getDate().toString()+"-"+(current_date.getMonth()+1).toString()+"-"+current_date.getFullYear().toString());
-	document.getElementById("end_date").value=document.getElementById("start_date").value; */
-  }
+// window.onload = function(){
+	// var current_date=new Date();
+	// document.getElementById("start_date").value=current_date.getDate().toString()+"-"+(current_date.getMonth()+1).toString()+"-"+current_date.getFullYear().toString());
+	// document.getElementById("end_date").value=document.getElementById("start_date").value;
+  // }
+  
+var dataset = [];
 
 format = function(x) {
 	return x[2]+"."+x[1]+'.'+x[0];
@@ -28,12 +29,16 @@ function month_length(m, y) {
  $(document).on('click', "#submit", function() {
 	var start_string=$("#start_date").val().split('-');
 	var end_string=$("#end_date").val().split('-');
+	for (var i=0; i<start_string.length; i++){
+		start_string[i] = parseInt(start_string[i]);
+		end_string[i] = parseInt(end_string[i]);
+	}
 	if ($("#course_name").val() !== '') {
 		if ( $("#end_date").val() === '' && $("#duration").val() === '') {
 			alert("You haven't specified end of the course, please fix it!");
 		}
-		else if($("#duration").val() === '' && new Date(start_string[0],start_string[1],start_string[2] )> new Date(end_string[0], end_string[1], end_string[2])) {
-			alert("End date cannot be earlier than Start date");
+		else if($("#duration").val() === '' && new Date(start_string[0],start_string[1]-1,start_string[2] )>= new Date(end_string[0], end_string[1]-1, end_string[2])) {
+			alert("End date should be later than Start date");
 		}
 		else { 
 			var new_course = {};
@@ -45,15 +50,47 @@ function month_length(m, y) {
 			}
 			else {
 				var duration = $("#duration").val() * 7;
-				
-				alert(duration);
+				var s_day = parseInt(new_course["start_date"][2]);
+				var s_month = parseInt(new_course["start_date"][1]);
+				var s_year = parseInt(new_course["start_date"][0]);
+				var days = month_length(s_month, s_year);
+				while (duration>(days-s_day+1)) {
+					if (s_month !== 12) {
+						s_month +=1;
+						duration = duration - (days - s_day + 1);
+						s_day = 1;
+						days = month_length(s_month, s_year);
+						alert(s_month);
+					}
+					else {
+						s_year += 1;
+						s_month = 1;
+						duration = duration - (days - s_day + 1);
+						s_day = 1;
+						days = month_length(s_month, s_year);
+					}
+				}
+				s_day += duration;
+				new_course["end_date"] = [s_year, s_month, s_day];
+				alert("You MOOC "+$("#course_name").val()+" was added");
 			}
-			alert("Month length: "+ month_length(parseInt(new_course["start_date"][1]), parseInt(new_course["start_date"][0])));
-			$("#result").append("<div>"+new_course["course_name"]+" Starts: "+format(new_course["start_date"])+" Ends: "+format(new_course["end_date"])+"</div>");
+
+			// alert(new Date(s_year, s_month-1, s_day));
+			// alert("Month length: "+ month_length(parseInt(new_course["start_date"][1]), parseInt(new_course["start_date"][0])));
+			// $("#result").append("<div>"+new_course["course_name"]+" Starts: "+format(new_course["start_date"])+" Ends: "+format(new_course["end_date"])+"</div>");
 			dataset.push(new_course);
-			}		
+			// $("#result").append(dataset[0].course_name)
+			}
 		 }
 	else {
 	alert("Please enter name of your course!");
 	}
 }) 
+
+$(document).on('click', "#show", function() {
+$("#result").append("<table><tr><th>Course name</th><th>Starts</th><th>Ends</th></tr>");
+	for (var i=0; i<dataset.length; i++) {
+		$("#result").append("<tr><td>"+dataset[i].course_name+"</td><td>"+format(dataset[i].start_date)+"</td><td>"+format(dataset[i].end_date)+"</td></tr>");
+	}
+$("#result").append("</table>");	
+})
