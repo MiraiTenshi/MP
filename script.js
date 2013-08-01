499a56177d3b055a8442b9d9def55f964e87be76
Astrelle <anyuta.se@gmail.com>
2013-08-01 11:01:44 +0300

    Update script.js
    Main update for today button and algorithm refactoring

--- script.js
+++ script.js
@@ -27,10 +27,11 @@ function month_length(m, y) {
 }
 
 function to_table(data) {
-	var result = '' 
+	var result = "<table rules='all' cellpadding='4'><tr><th>Course name</th><th>Starts</th><th>Ends</th></tr>";
 	for (var i=0; i<data.length; i++) {
 		result+="<tr><td>"+data[i].course_name+"</td><td>"+format(data[i].start_date)+"</td><td>"+format(data[i].end_date)+"</td></tr>";
 	}
+	result+="</table>";
 	return result
 }
   
@@ -45,51 +46,50 @@ function to_table(data) {
 		if ( $("#end_date").val() === '' && $("#duration").val() === '') {
 			alert("You haven't specified end of the course, please fix it!");
 		}
-		else if($("#duration").val() === '' && new Date(start_string[0],start_string[1]-1,start_string[2] )> new Date(end_string[0], end_string[1]-1, end_string[2])) {
-			alert("End date should be later than Start date");
-		}
-		else { 
+		else {
 			var new_course = {};
 			new_course["course_name"]=($("#course_name").val());
-			new_course["start_date"]=start_string; 
-			if ($("#end_date").val() !== '' && $("#end_date").val() !== $("#start_date").val()){
+			new_course["start_date"]=start_string;
+			if ( $('#end_date').val !=='' && new Date(start_string[0],start_string[1]-1,start_string[2] )<= new Date(end_string[0], end_string[1]-1, end_string[2])) {
 				new_course["end_date"]=end_string;
 				alert('You course: "'+$("#course_name").val()+'" was added');
 				document.getElementById("end_date").value='';
+				dataset.push(new_course);
 			}
-			else {
-				var duration = $("#duration").val() * 7;
-				var s_day = parseInt(new_course["start_date"][2]);
-				var s_month = parseInt(new_course["start_date"][1]);
-				var s_year = parseInt(new_course["start_date"][0]);
-				var days = month_length(s_month, s_year);
-				while (duration>(days-s_day+1)) {
-					if (s_month !== 12) {
-						s_month +=1;
-						duration = duration - (days - s_day + 1);
-						s_day = 1;
-						days = month_length(s_month, s_year);
+			else if ($('#end_date').val() ==='' || new Date(start_string[0],start_string[1]-1,start_string[2] )> new Date(end_string[0], end_string[1]-1, end_string[2])) {
+				if ($('#duration').val() !==''){
+					var duration = $("#duration").val() * 7;
+					var s_day = parseInt(new_course["start_date"][2]);
+					var s_month = parseInt(new_course["start_date"][1]);
+					var s_year = parseInt(new_course["start_date"][0]);
+					var days = month_length(s_month, s_year);
+					while (duration>(days-s_day+1)) {
+						if (s_month !== 12) {
+							s_month +=1;
+							duration = duration - (days - s_day + 1);
+							s_day = 1;
+							days = month_length(s_month, s_year);
+						}
+						else {
+							s_year += 1;
+							s_month = 1;
+							duration = duration - (days - s_day + 1);
+							s_day = 1;
+							days = month_length(s_month, s_year);
+						}
 					}
-					else {
-						s_year += 1;
-						s_month = 1;
-						duration = duration - (days - s_day + 1);
-						s_day = 1;
-						days = month_length(s_month, s_year);
-					}
+					s_day += duration;
+					new_course["end_date"] = [s_year, s_month, s_day];
+					alert('You course: "'+$("#course_name").val()+'" was added');
+					document.getElementById("end_date").value='';
+					dataset.push(new_course);
 				}
-				s_day += duration;
-				new_course["end_date"] = [s_year, s_month, s_day];
-				alert('You course: "'+$("#course_name").val()+'" was added');
-				document.getElementById("end_date").value='';
+				else {
+					alert("End date should be later than Start date");
+				}
 			}
-
-			// alert(new Date(s_year, s_month-1, s_day));
-			// alert("Month length: "+ month_length(parseInt(new_course["start_date"][1]), parseInt(new_course["start_date"][0])));
-			// $("#result").append("<div>"+new_course["course_name"]+" Starts: "+format(new_course["start_date"])+" Ends: "+format(new_course["end_date"])+"</div>");
-			dataset.push(new_course);
-			}
-		 }
+		}
+	}
 	else {
 	alert("Please enter name of your course!");
 	}
@@ -97,17 +97,18 @@ function to_table(data) {
 
 $(document).on('click', "#show", function() {
 	$("#result").empty();
-	$("#result").append("<table rules='all' cellpadding='4'><tr><th>Course name</th><th>Starts</th><th>Ends</th></tr>"+to_table(dataset)+"</table>");
+	if (dataset.length !== 0) {$("#result").append(to_table(dataset))}
 })
 $(document).on('click', "#today", function() {
 	$("#result").empty();
 	today_table =[];
 	for (var i=0; i<dataset.length; i++) {
-		if (new Date(dataset[i].start_date[0], dataset[i].start_date[1]-1, dataset[i].start_date[2])<=new Date() && new Date(dataset[i].end_date[0], dataset[i].end_date[1]-1, dataset[i].end_date[2])>=new Date()) {
+		if (new Date(dataset[i].start_date[0], dataset[i].start_date[1]-1, dataset[i].start_date[2])<=new Date() && new Date(dataset[i].end_date[0], dataset[i].end_date[1]-1, dataset[i].end_date[2], 23)>=new Date()) {
 			// alert(new Date(dataset[i].start_date[0], dataset[i].start_date[1]-1, dataset[i].start_date[2])+" "+new Date());
 			today_table.push(dataset[i]);
 		}
-	}
+		// else {alert(new Date(dataset[i].start_date[0], dataset[i].start_date[1]-1, dataset[i].start_date[2])+" "+new Date());}
+	// }
 	// for (var i=0; i<today_table.length; i++) {alert(today_table[i].course_name);}
-	$("#result").append("<table rules='all' cellpadding='4'><tr><th>Course name</th><th>Starts</th><th>Ends</th></tr>"+to_table(today_table)+"</table>");
+	if (today_table.length !== 0) {$("#result").append(to_table(today_table))}
 })
